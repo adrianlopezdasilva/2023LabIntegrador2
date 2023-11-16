794316f3451fbb5314fdb710fc5540b4209b4659
adrianlopezdasilva <40776599+adrianlopezdasilva@users.noreply.github.com>
2023-11-16 18:43:46 -0300

    Commit inicial

---
+++ LopezDaSilva.Adrian.2C.TPFinal/Cementerio/Persona.cs
@@ -0,0 +1,25 @@
+﻿namespace Cementerio
+{
+    public abstract class Persona
+    {
+		protected string nombre;
+        protected int edad;
+
+		public int Edad
+		{
+			get { return edad; }
+
+		}
+
+		public string Nombre
+		{
+			get { return nombre; }
+		}
+
+		public Persona (string nombre, int edad)
+		{
+			this.nombre = nombre;
+			this.edad = edad;
+		}
+	}
+}
